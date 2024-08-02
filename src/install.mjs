8e7c1a0b58811252a3d2592156185b19c7fef978
Niklas Widmann <nik@somethingcatchy.net>
2024-08-02 10:39:39 +0200

    add support for curseforge downloads

--- src/install.mjs
+++ src/install.mjs
@@ -22,6 +22,51 @@ function verifySha(name, content, checksum) {
    }
 }
 
+async function fetchFromCurseforge(endpoint) {
+   const token = process.env.CURSEFORGE_TOKEN
+
+   if (!token) throw new Error(`Unable to download from curseforge without passing a token`)
+
+   const response = await fetch(`https://api.curseforge.com/${endpoint}`, {
+      headers: {
+         'x-api-key': token,
+      },
+   })
+   if (!response.ok) throw new Error(response.statusText)
+
+   return response
+}
+
+async function getUrlFrom({ download, name, update }) {
+   if (download.url) return download.url
+   if (download.mode === 'metadata:curseforge' && update.curseforge) {
+      const response = await fetchFromCurseforge(
+         `v1/mods/${update.curseforge['project-id']}/files/${update.curseforge['file-id']}/download-url`
+      )
+      const { data } = await response.json()
+      return data
+   }
+
+   throw new Error(`unable to find url for ${name}`)
+}
+
+async function downloadFile(definition, outPath) {
+   if (!existsSync(dirname(outPath))) mkdirSync(dirname(outPath), { recursive: true })
+
+   const url = await getUrlFrom(definition)
+   const response = await fetch(url)
+   if (!response.ok) throw new Error(response.statusText)
+
+   const out = createWriteStream(outPath)
+
+   await new Promise((res, rej) => {
+      const readable = Readable.fromWeb(response.body)
+      readable.pipe(out)
+      readable.on('error', rej)
+      readable.on('end', res)
+   })
+}
+
 async function installPack(from, to, options) {
    if (!from) throw new Error('input argument missing')
    if (!to) throw new Error('output argument missing')
@@ -41,7 +86,7 @@ async function installPack(from, to, options) {
 
    const index = readToml(join(packDir, pack.index.file))
 
-   await Promise.all(
+   const results = await Promise.allSettled(
       index.files.map(async ({ file }) => {
          const definition = readToml(join(packDir, file))
 
@@ -50,17 +95,7 @@ async function installPack(from, to, options) {
          const outPath = join(to, dirname(file), definition.filename)
 
          if (!existsSync(outPath) || options.includes('--overwrite')) {
-            if (!existsSync(dirname(outPath))) mkdirSync(dirname(outPath), { recursive: true })
-
-            const out = createWriteStream(outPath)
-            const response = await fetch(definition.download.url)
-
-            await new Promise((res, rej) => {
-               const readable = Readable.fromWeb(response.body)
-               readable.pipe(out)
-               readable.on('error', rej)
-               readable.on('end', res)
-            })
+            await downloadFile(definition, outPath)
          } else {
             console.log(`   using cached file for ${outPath}`)
          }
@@ -68,6 +103,13 @@ async function installPack(from, to, options) {
          verifyFileSha(outPath, definition.download.hash)
       })
    )
+
+   const errors = results.filter(it => it.status === 'rejected')
+
+   if (errors.length > 0) {
+      errors.forEach(it => console.error(it.reason))
+      throw new Error(`${errors.length} files failed to download`)
+   }
 }
 
 const args = process.argv.slice(2)
