de246596f8027431735da851c46e75b58175901a
Niklas Widmann <nik@somethingcatchy.net>
2024-08-07 12:05:08 +0200

    fixed even better advanced search

--- web/src/components/IconPanel.tsx
+++ web/src/components/IconPanel.tsx
@@ -1,43 +1,23 @@
-import type { FuseResult, FuseResultMatch, RangeTuple } from 'fuse.js'
+import type { FuseResult, FuseResultMatch } from 'fuse.js'
 import { useCallback, useMemo } from 'react'
 import type { Icon } from '../types/Icon.ts'
 
 import styles from './IconPanel.module.css'
 
-function getIndices(match?: FuseResultMatch): RangeTuple[] {
-   const indices: RangeTuple[] = []
-
-   if (match) {
-      match.indices
-         .toSorted((a, b) => b[1] - b[0] - (a[1] - a[0]))
-         .forEach(([from, to]) => {
-            if (
-               !indices.some(([from2, to2]) => {
-                  return from2 <= from && to2 >= to
-               })
-            ) {
-               indices.push([from, to])
-            }
-         })
-   }
-
-   return indices
-}
-
 function Highlighted({ children, match }: { match?: FuseResultMatch | undefined; children: string }) {
    const slices = useMemo(() => {
-      const indices = getIndices(match)
+      const indices = match?.indices ?? []
       const firstMatch = indices[0]?.[0] ?? children.length
 
       return [
          <span key='start'>{children.slice(0, firstMatch)}</span>,
          ...indices.flatMap(([from, to], i, a) => {
-            const nextMatch = a[i + 1]?.[1] ?? children.length + 1
+            const nextMatch = a[i + 1]?.[0] ?? children.length
             return [
                <span key={`match-${i}`} className={styles.highlighted}>
                   {children.slice(from, to + 1)}
                </span>,
-               <span key={`no-match-${i}`}>{children.slice(to + 1, nextMatch - 1)}</span>,
+               <span key={`no-match-${i}`}>{children.slice(to + 1, nextMatch)}</span>,
             ]
          }),
       ]
