0edb5569764f21c6b179872d4e3b69e21947909d
Niklas Widmann <nik@somethingcatchy.net>
2024-08-07 10:10:35 +0200

    filter using registry dump

--- generator/src/main.mjs
+++ generator/src/main.mjs
@@ -1,11 +1,26 @@
 import { renderFrom } from '@pssbletrngle/assets-renderer'
+import { existsSync, readdirSync, readFileSync } from 'fs'
+import { join } from 'path'
+
+function parseDumps(from) {
+   if (!existsSync(from)) throw new Error(`registry dump directory missing`)
+
+   const registries = readdirSync(from)
+
+   return registries.flatMap(registry => {
+      const namespaces = readdirSync(join(from, registry))
+
+      return namespaces.flatMap(namespace => {
+         const content = readFileSync(join(from, registry, namespace))
+         return JSON.parse(content.toString())
+      })
+   })
+}
 
 async function run() {
-   await renderFrom(
-      ['../resources', '../install/mods'],
-      { output: '../web/public/icons', keep: true },
-      { exclude: 'assets_renderer:*' }
-   )
+   const include = parseDumps('../dump')
+
+   await renderFrom(['../resources', '../install/mods'], { output: '../web/public/icons', keep: true }, { include })
 }
 
 run().catch(e => {
